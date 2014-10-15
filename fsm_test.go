e4d798f721f623636c02bf4c24406f5b05f15de7
Ryan Norris <rynorris@gmail.com>
2014-10-15 19:09:58 +0100

    Brought up to 100% test coverage

--- fsm_test.go
+++ fsm_test.go
@@ -1,8 +1,6 @@
 package fsm
 
-import (
-	"testing"
-)
+import "testing"
 
 const (
 	test_input_1 = iota
@@ -75,4 +73,110 @@ func TestStates(t *testing.T) {
 
 	t.Log("3: 1 -> 1")
 	assertState(t, fsm, test_input_3, test_state_1)
+}
+
+// Test actions which return next inputs.
+func TestChain(t *testing.T) {
+	func1_hit := false
+	func2_hit := false
+	func3_hit := false
+	state1 := State{
+		Index: test_state_1,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_2,
+				func() Input { func1_hit = true; return test_input_2 }},
+			test_input_2: Outcome{test_state_3,
+				func() Input { func3_hit = true; return NO_INPUT }},
+			test_input_3: Outcome{test_state_1, NO_ACTION},
+		},
+	}
+	state2 := State{
+		Index: test_state_2,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_1, NO_ACTION},
+			test_input_2: Outcome{test_state_1,
+				func() Input { func2_hit = true; return test_input_2 }},
+			test_input_3: Outcome{test_state_1, NO_ACTION},
+		},
+	}
+	state3 := State{
+		Index: test_state_3,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_2, NO_ACTION},
+			test_input_2: Outcome{test_state_1, NO_ACTION},
+			test_input_3: Outcome{test_state_1, NO_ACTION},
+		},
+	}
+
+	fsm := Define(state1, state2, state3)
+
+	// Spin with input 1.
+	// The state chain should go 1 -> 2 -> 1 -> 3 and all 3 functions should have been hit.
+	assertState(t, fsm, test_input_1, test_state_3)
+	if !(func1_hit && func2_hit && func3_hit) {
+		t.Errorf("Didn't hit all 3 actions. 1: %v, 2: %v, 3: %v.", func1_hit, func2_hit, func3_hit)
+	}
+}
+
+func TestImpossibleState(t *testing.T) {
+	state1 := State{
+		Index: test_state_1,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_2, NO_ACTION},
+			test_input_2: Outcome{test_state_3, NO_ACTION},
+		},
+	}
+	state2 := State{
+		Index: test_state_2,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_1, NO_ACTION},
+			test_input_2: Outcome{test_state_1, NO_ACTION},
+		},
+	}
+
+	fsm := Define(state1, state2)
+
+	// Put the FSM in an impossible state.
+	fsm.current = test_state_3
+	err := fsm.Spin(test_input_1)
+	if err == nil {
+		t.Fatalf("FSM didn't error when spun in impossible state.")
+	}
+	switch err.(type) {
+	case ImpossibleStateError:
+		t.Log("FSM corrently returned error: %v", err.Error())
+	default:
+		t.Fatalf("FSM returned wrong error type: %T", err)
+	}
+}
+
+func TestInvalidInput(t *testing.T) {
+	state1 := State{
+		Index: test_state_1,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_2, NO_ACTION},
+			test_input_2: Outcome{test_state_3, NO_ACTION},
+		},
+	}
+	state2 := State{
+		Index: test_state_2,
+		Outcomes: map[Input]Outcome{
+			test_input_1: Outcome{test_state_1, NO_ACTION},
+			test_input_2: Outcome{test_state_1, NO_ACTION},
+		},
+	}
+
+	fsm := Define(state1, state2)
+
+	// Spin with invalid input value.
+	err := fsm.Spin(test_input_3)
+	if err == nil {
+		t.Fatalf("FSM didn't error when spun with invalid input.")
+	}
+	switch err.(type) {
+	case InvalidInputError:
+		t.Log("FSM corrently returned error: %v", err.Error())
+	default:
+		t.Fatalf("FSM returned wrong error type: %T", err)
+	}
 }
